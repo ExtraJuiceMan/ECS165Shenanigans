3727e0244a9a5c08c9806c1f90f23fee9c9d33f6
Jade Elkins <jadenelkins@gmail.com>
2023-02-12 18:19:28 -0800

    Oops forgot to format.

--- crabstore/src/lib.rs
+++ crabstore/src/lib.rs
@@ -108,15 +108,23 @@ impl Index {
             }),
         }
     }
-    pub fn range_from_index(&self, column_number: usize, begin:i64, end: i64) -> Option<Vec<&RID>>{
-        match &self.indices[column_number]{
+    pub fn range_from_index(
+        &self,
+        column_number: usize,
+        begin: i64,
+        end: i64,
+    ) -> Option<Vec<&RID>> {
+        match &self.indices[column_number] {
             None => None,
-            Some(map) =>Some({
-                let rids:Vec<RID> = Vec::new();
-                let rids = map.range(begin..end)
-                    .map(|item|item.1).flatten().collect::<Vec<&RID>>();
+            Some(map) => Some({
+                let rids: Vec<RID> = Vec::new();
+                let rids = map
+                    .range(begin..end)
+                    .map(|item| item.1)
+                    .flatten()
+                    .collect::<Vec<&RID>>();
                 rids
-            })
+            }),
         }
     }
     pub fn create_index(&mut self, column_number: usize) {
