8d9ad60b553d4c67c5f6da4790dfe1bdd479c584
Jade Elkins <jadenelkins@gmail.com>
2023-03-15 23:29:39 -0700

    Oops.

--- crabstore/src/transaction_worker.rs
+++ crabstore/src/transaction_worker.rs
@@ -1,6 +1,11 @@
-use crate::transaction::Transaction;
+use std::sync::Arc;
+
+use crate::{
+    table::{Table, TableData},
+    transaction::Transaction,
+};
 #[derive(Debug)]
 pub struct TransactionWorker {
     transactions: Vec<Transaction>,
-    table: Arc<Table>,
+    table: Arc<TableData>,
 }
